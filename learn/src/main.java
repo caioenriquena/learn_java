da1129ba3ad5fefa6c6ea12ec2e34112e1382114
caioenriquena <caioenrique.na@gmail.com>
2023-12-08 10:27:18 -0300

    dowhile

--- learn/src/main.java
+++ learn/src/main.java
@@ -2,10 +2,17 @@ public class main {
     public static void main(String[] args) {
         int idade = 0;
 
-        while (idade <= 10) {
+        while (idade <= 10) { /*verifica a condição e depois executa */
             System.out.println("minha idade começou de  :" + idade);
             idade ++;
             
+            
         }
+        /*executa e depois verifica a condiçao */
+        int idade2 = 0;
+        do {
+            System.out.println("minha idade começou de  :" + idade2);
+            idade2 ++;
+        } while (idade2 <= 60);
     }
 }
