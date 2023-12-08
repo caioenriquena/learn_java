88f79786eed1ed77a363d3f37da4d86cbb899fc1
caioenriquena <caioenrique.na@gmail.com>
2023-12-08 10:15:13 -0300

    while

---
+++ learn/src/main.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        int idade = 0;
+
+        while (idade <= 10) {
+            System.out.println("minha idade começou de  :" + idade);
+            idade ++;
+            
+        }
+    }
+}
