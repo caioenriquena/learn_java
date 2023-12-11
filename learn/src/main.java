d49b3799c6c5ef3d91a7453971e2c2e7355c627b
Caio Enrique Neves Araújo <caioenrique.na@gmail.com>
2023-12-11 13:10:33 -0300

    for

--- learn/src/main.java
+++ learn/src/main.java
@@ -1,18 +1,24 @@
 public class main {
     public static void main(String[] args) {
+        /*verifica a condição e depois executa
         int idade = 0;
-
-        while (idade <= 10) { /*verifica a condição e depois executa */
+        while (idade <= 10) {
             System.out.println("minha idade começou de  :" + idade);
             idade ++;
-            
-            
-        }
-        /*executa e depois verifica a condiçao */
+
+
+        } */
+        /*executa e depois verifica a condiçao
         int idade2 = 0;
         do {
             System.out.println("minha idade começou de  :" + idade2);
             idade2 ++;
-        } while (idade2 <= 60);
+        } while (idade2 <= 60); */
+
+
+        for (int numero= 0; numero <= 10; numero++) {
+            System.out.println("o numero atual é " + numero);
+        }
+
     }
 }
