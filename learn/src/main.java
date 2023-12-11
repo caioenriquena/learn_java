b36f7218e9a0ac88e1cb2b779f12c97a521caeee
Caio Enrique Neves Araújo <caioenrique.na@gmail.com>
2023-12-11 13:41:08 -0300

    exercicios for

--- learn/src/main.java
+++ learn/src/main.java
@@ -16,9 +16,52 @@ public class main {
         } while (idade2 <= 60); */
 
 
-        for (int numero= 0; numero <= 10; numero++) {
-            System.out.println("o numero atual é " + numero);
+        // exercicios com for
+
+        // ex 1
+
+        // Escreva um programa em Java que imprima os números de 1 a 100, um por linha.
+
+
+        for (int i = 0; i <= 100 ; i++)  {
+            System.out.println("os numeros de 1  a 100 sao : " + i);
         }
 
+        // ex 2
+
+        // Escreva um programa em Java que imprima os números pares de 1 a 100, um por linha.
+
+        for (int i = 0; i <= 100 ; i++) {
+            if (i % 2 ==0){
+                System.out.println("os numeros pares de 1 a 100 sao :" + i);
+            }
+        }
+
+        // ex 3
+
+        // Escreva um programa em Java que imprima a soma dos números de 1 a 100.
+        int soma = 0;
+        for (int i = 1; i <= 100; i++  ){
+            soma  += i;
+            {
+                System.out.println(" a soma dos valores de  1 a 100 é : " + soma);
+            }
+        }
+        // ex 4
+        // Escreva um programa em Java que imprima os números primos de 1 a 100.
+        for (int i = 2; i <= 100; i++) {
+            boolean primo = true;
+
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    primo = false;
+                    break;
+                }
+            }
+
+            if (primo) {
+                System.out.println(i);
+            }
+        }
     }
 }
