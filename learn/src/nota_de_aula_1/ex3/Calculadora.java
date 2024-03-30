467297039e149bd0c20883273a63d66ffbb27b3c
Caio Enrique Neves Araújo <caioenrique.na@gmail.com>
2024-03-30 15:06:25 -0300

    nota aula 1

---
+++ learn/src/nota_de_aula_1/ex3/Calculadora.java
@@ -0,0 +1,25 @@
+package nota_de_aula_1.ex3;
+
+public class Calculadora {
+    int total;
+
+
+
+    public int somar(int n1, int n2){
+        total = n1 + n2;
+        return total;
+    }
+    public int subtracao(int n1, int n2){
+        total = n1 - n2;
+        return total;
+    }
+    public int divisao(int n1, int n2){
+        total = n1 / n2;
+        return total;
+    }
+    public int multiplicacao(int n1, int n2){
+        total = n1 * n2;
+        return total;
+    }
+}
+
